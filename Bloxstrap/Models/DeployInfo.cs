cefc176c0382793e91ff40c4535012c35555c0cb
i_iMikey <67828573+MikeyUsersREC@users.noreply.github.com>
2024-04-05 06:10:32 +0100

    Update DeployInfo.cs

--- Bloxstrap/Models/DeployInfo.cs
+++ Bloxstrap/Models/DeployInfo.cs
@@ -3,6 +3,7 @@
     public class DeployInfo
     {
         public string Timestamp { get; set; } = null!;
+        public string YourVersion { get; set; } = null!;
         public string Version { get; set; } = null!;
         public string VersionGuid { get; set; } = null!;
     }
