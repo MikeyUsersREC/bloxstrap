91c3c5ec077d2fab69cb6592ba10570f955642a0
i_iMikey <67828573+MikeyUsersREC@users.noreply.github.com>
2024-04-05 06:11:47 +0100

    Update BehaviourViewModel.cs

--- Bloxstrap/UI/ViewModels/Menu/BehaviourViewModel.cs
+++ Bloxstrap/UI/ViewModels/Menu/BehaviourViewModel.cs
@@ -39,7 +39,7 @@
                 {
                     Version = info.Version,
                     VersionGuid = info.VersionGuid,
-                    YourVerion = version,
+                    YourVersion = version,
                     Timestamp = info.Timestamp?.ToFriendlyString()!
                 };
 
